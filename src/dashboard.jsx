cd167f0981a438274925794bd182583d677ff741
Xpression <emmybanks03@gmail.com>
2025-05-12 01:03:41 +0100

    refactor: implement category selection for news articles and enhance toolbar functionality

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,4 +1,4 @@
-import { FaBars } from "react-icons/fa"
+import { FaBars, FaTimes} from "react-icons/fa"
 import { getData } from "country-list"
 import ISO6391 from 'iso-639-1';
 import { useState, useEffect } from "react";
@@ -15,6 +15,8 @@ const languages = ISO6391.getAllNames().map((name) => {
     };
   });
 
+  
+  const lists = ['News','Sport','Entertainment','Music','Lifestyle','Politics','Education','Health','Food','Tech','Finance','Religion'];
 
   
 
@@ -22,8 +24,11 @@ export const Dashboard = () => {
     const [context, setContext] = useState([]);
     const [selectedCountry, setSelectedCountry] = useState(localStorage.getItem('selectedCountry') || 'us');
     const [selectedLang, setSelectedLang] = useState('en');
+    const [newsList, setNewsList] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState("news"); // Default to "news"
 
-    const url = `https://newsdata.io/api/1/latest?apikey=pub_8471398a18d77380aac2ccf2abf1292a1b65e&q=news&country=${selectedCountry}&language=${selectedLang}`;
+
+    const url = `https://newsdata.io/api/1/latest?apikey=pub_8471398a18d77380aac2ccf2abf1292a1b65e&q=${selectedCategory}&country=${selectedCountry}&language=${selectedLang}`;
     
       useEffect(()=>{
         console.log("Selected Country:", selectedCountry);
@@ -43,7 +48,7 @@ export const Dashboard = () => {
             console.log("Fetched Data:", data);
             setContext(data.results);
 
-            // Translate the content
+            // Translate content
             const translatedResults = await Promise.all(
                 data.results.map(async (article) => {
                     const res = await fetch("https://libretranslate.de/translate", {
@@ -72,11 +77,11 @@ export const Dashboard = () => {
 };
 
         request();
-      },[selectedCountry, selectedLang]);
+      },[selectedCategory, selectedCountry, selectedLang]);
 
 
       useEffect(()=>{
-        if(!localStorage.getItem('selectedCountry')){
+        if(!localStorage.getItem('selectedCountry') & navigator.onLine){
              //get users current country location to fecth news from there
             if(navigator.geolocation){
                 navigator.geolocation.getCurrentPosition((position)=>{
@@ -96,15 +101,21 @@ export const Dashboard = () => {
         }
       }, [])
 
-      useEffect(()=>{
-
-      })
-
     return (
         <>
         <div className="nav">
+            {newsList? 
+            <div className="toolBar"  style={{ width: newsList ? "250px" : "0" }}>
+                <FaTimes className="close" onClick={() => setNewsList(prevState => !prevState)} />
+                    <p>Choose your area of interest...</p>
+            <ul className="newsList">
+            {lists.map((e, index)=>(
+                <li key={index} onClick={() => setSelectedCategory(e.toLowerCase())}>{e}</li>
+            ))}
+            </ul>
+            </div> : null }
             <div style={{display:'flex', justifyContent:'center', alignItems:'center', gap:30}}>
-            <FaBars />
+            <FaBars onClick={() => setNewsList(prevState => !prevState)}/>
             <h1>Multilingua News Aggregator</h1>
             </div>
             <select className="selection" value={selectedCountry} onChange={(e)=> setSelectedCountry (e.target.value)}>
